0bed4761a41fc768f009aa414e5c8d2008ff3b2a
Leonardo Porro <mail@leonardoporro.com.ar>
2021-03-16 13:46:26 -0300

    first version as a standalone library

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# Detached-Types
+# Runtime Types
+#### What is it
+
+This library tries to simplify the runtime type creation by adding new things like the ability to define methods using Expressions and 
+automatic interface implementation.
+It's a part of Detached.Mappers library.
+Thanks to the people who made [FastExpressionCompiler](https://github.com/dadhi/FastExpressionCompiler), that is the core of this library.
+
+## What does it solve
+ It allows devs to create tools like dynamic proxies, comparers, dirty check (INotifyPropertyChange) or any other thing 
+ that may be a good fit for dynamic code without having to manually emit op codes. Methods can be defined using Expression trees.
+
+### How it works
+
+´´´csharp
+// create a type builder, this will handle the creation of a new Type.
+RuntimeTypeBuilder typeBuilder = new RuntimeTypeBuilder("MyISumServiceImplementation", typeof(BasePropertyClass));
+
+// define the parameters expected by the interface method.
+var aParam = Parameter(typeof(int), "a");
+var bParam = Parameter(typeof(int), "b");
+
+// define a mehtod with the same signature (name and parameters),
+// use "Add" expression as the method body
+typeBuilder.DefineMethod(
+    "Sum",
+        new[] { aParam, bParam },
+        Block(
+        Add(aParam, bParam)
+        )
+);
+            
+// implement the interface, this will iterate over all methods and 
+// call DefineMethodOverride to bind the existing methods to the given 
+// interface methods
+typeBuilder.AutoImplementInterface(typeof(ISumService));
+
+// create the type that will be used to initialize new instances of ISumService
+Type myISumServiceType = typeBuilder.Create();
+
+// create an instance of our dynamic ISumService implementation
+ISumService myISumService = (ISumService)Activator.CreateInstance(myISumServiceType);
+
+// test the implementation
+Assert.Equal(14, myISumService.Sum(5, 9));
+
+´´´
+
+More info and examples will be added later.
+Check unit tests for more samples!
+
+### NOTE: This library is in an early stage, so that, side effects of compiled types may occur and are yet undeceted.
+### Any help on debugging, or adding new features is very welcome!
---
+++ test/Detached.RuntimeTypes.Tests/RuntimeTypeTests.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using Xunit;
+using static Detached.RuntimeTypes.Expressions.ExtendedExpression;
+using static System.Linq.Expressions.Expression;
+
+namespace Detached.RuntimeTypes.Tests
+{
+    public class RuntimeTypeTests
+    {
+        [Fact]
+        public void define_autoproperty()
+        {
+            RuntimeTypeBuilder typeBuilder = new RuntimeTypeBuilder("DefineAutoProperty");
+            typeBuilder.DefineAutoProperty("TestProp", typeof(int));
+
+            Type newType = typeBuilder.Create();
+            object newInstance = Activator.CreateInstance(newType);
+
+            PropertyInfo testPropInfo = newType.GetProperty("TestProp");
+            testPropInfo.SetValue(newInstance, 5);
+            int result = (int)testPropInfo.GetValue(newInstance);
+
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void define_method()
+        {
+            RuntimeTypeBuilder typeBuilder = new RuntimeTypeBuilder("DefineMethod");
+            ParameterExpression a = Parameter(typeof(int), "a");
+            ParameterExpression b = Parameter(typeof(int), "b");
+
+            typeBuilder.DefineMethod("TestMethod",
+                new[] { a, b },
+                Add(a, b)
+            );
+
+            Type newType = typeBuilder.Create();
+            object newInstance = Activator.CreateInstance(newType);
+
+            MethodInfo testMethodInfo = newType.GetMethod("TestMethod");
+            object result = testMethodInfo.Invoke(newInstance, new object[] { 5, 4 });
+
+            Assert.Equal(9, result);
+        }
+
+        [Fact]
+        public void access_local_field()
+        {
+            RuntimeTypeBuilder typeBuilder = new RuntimeTypeBuilder("AccessLocalField");
+
+            var field = typeBuilder.DefineField("TestField", typeof(string));
+
+            typeBuilder.DefineMethod("TestMethod",
+                null,
+                Field(typeBuilder.This, field)
+            );
+
+            Type newType = typeBuilder.Create();
+            object newInstance = Activator.CreateInstance(newType);
+
+            FieldInfo fieldInfo = newType.GetField("TestField");
+            fieldInfo.SetValue(newInstance, "testValue");
+            MethodInfo testMethodInfo = newType.GetMethod("TestMethod");
+            object result = testMethodInfo.Invoke(newInstance, null);
+
+            Assert.Equal("testValue", result);
+        }
+
+        [Fact]
+        public void call_base_method()
+        {
+            RuntimeTypeBuilder typeBuilder = new RuntimeTypeBuilder("CallBaseMethod", typeof(BaseMethodClass));
+            MethodInfo getTextInfo = typeof(BaseMethodClass).GetMethod("GetText");
+
+            typeBuilder.OverrideMethod(getTextInfo,
+                null,
+                Call("Concat", typeof(string), Call(typeBuilder.Base, getTextInfo), Constant(" (overriden)"))
+            );
+
+            Type newType = typeBuilder.Create();
+            object newInstance = Activator.CreateInstance(newType);
+
+            MethodInfo testMethodInfo = newType.GetMethod("GetText");
+            object result = testMethodInfo.Invoke(newInstance, null);
+
+            Assert.Equal("this is the base class! (overriden)", result);
+        }
+
+        [Fact]
+        public void override_property()
+        {
+            RuntimeTypeBuilder typeBuilder = new RuntimeTypeBuilder("OverrideProperty", typeof(BasePropertyClass));
+
+            PropertyInfo propInfo = typeof(BasePropertyClass).GetProperty("Text");
+
+            typeBuilder.OverrideMethod(propInfo.GetGetMethod(),
+                null,
+                Call("Concat", typeof(string), Call(typeBuilder.Base, propInfo.GetGetMethod()), Constant(" (overriden)"))
+            );
+
+            Type newType = typeBuilder.Create();
+            object newInstance = Activator.CreateInstance(newType);
+
+            propInfo.SetValue(newInstance, "this is a property!");
+            string result = (string)propInfo.GetValue(newInstance);
+
+            Assert.Equal("this is a property! (overriden)", result);
+        }
+
+        [Fact]
+        public void auto_implement_interface()
+        {
+            RuntimeTypeBuilder typeBuilder = new RuntimeTypeBuilder("AutoImplenetInterface", typeof(BasePropertyClass));
+            typeBuilder.AutoImplementInterface(typeof(ITextProperty));
+
+            Type newType = typeBuilder.Create();
+            ITextProperty newInstance = (ITextProperty)Activator.CreateInstance(newType);
+
+            newInstance.Text = "test text";
+
+            Assert.Equal("test text", newInstance.Text);
+        }
+
+        [Fact]
+        public void build_implementation()
+        {
+            // create a type builder, this will handle the creation of 
+            // a new Type.
+            RuntimeTypeBuilder typeBuilder = new RuntimeTypeBuilder("MyISumServiceImplementation", typeof(BasePropertyClass));
+
+            // define the parameters expected by the interface method.
+            var aParam = Parameter(typeof(int), "a");
+            var bParam = Parameter(typeof(int), "b");
+
+            // define a mehtod with the same signature (name and parameters),
+            // use "Add" expression as the method body
+            typeBuilder.DefineMethod(
+                "Sum",
+                 new[] { aParam, bParam },
+                 Block(
+                    Add(aParam, bParam)
+                 )
+            );
+            
+            // implement the interface, this will iterate over all methods and 
+            // call DefineMethodOverride to bind the existing methods to the given 
+            // interface methods
+            typeBuilder.AutoImplementInterface(typeof(ISumService));
+
+            // create the type that will be used to initialize new instances of ISumService
+            Type myISumServiceType = typeBuilder.Create();
+
+            // create an instance of our dynamic ISumService implementation
+            ISumService myISumService = (ISumService)Activator.CreateInstance(myISumServiceType);
+
+            // test the implementation
+            Assert.Equal(14, myISumService.Sum(5, 9));
+        }
+
+        public interface ISumService
+        {
+            int Sum(int a, int b);
+        }
+
+        public class BasePropertyClass
+        {
+            public virtual string Text { get; set; }
+        }
+
+        public interface ITextProperty
+        {
+            string Text { get; set; }
+        }
+
+        public class BaseMethodClass
+        {
+            public virtual string GetText() => "this is the base class!";
+        }
+    }
+}
